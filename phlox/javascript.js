a0aed2bc679e9a413b87fbf0645b4edfd40bbd44
unknown <kameliacami15@gmail.com>
2019-12-13 17:41:17 +0200

    app-landing

---
+++ phlox/javascript.js
@@ -0,0 +1,27 @@
+/* button learn color*/
+function mouseOver() {
+  document.getElementById("demo").style.backgroundColor = "#FFFFFF";
+  document.getElementById("demo").style.color = "#000000";
+}
+
+function mouseOut() {
+  document.getElementById("demo").style.backgroundColor = "#EC2855";
+  document.getElementById("demo").style.color = "#FFFFFF";
+}
+
+/* slide*/
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("phone-image");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 2000);
+}
