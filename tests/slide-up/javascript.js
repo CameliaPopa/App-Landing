7bb06d9b24f0144f71978ccda7895f3e43755571
unknown <kameliacami15@gmail.com>
2019-12-19 17:49:19 +0200

    commit

---
+++ tests/slide-up/javascript.js
@@ -0,0 +1,47 @@
+$( document ).ready( function() {
+    var button = $('.button');
+    var slider = $('.slider');
+    button.on('click', function(e) {
+        if ( slider.hasClass('closed') ) {
+            slider.toggleClass('closed');
+        } else {
+            slider.toggleClass('closed');
+        }
+    });
+});
+
+$( document ).ready( function() {
+    var button = $('.button1');
+    var slider = $('.slider1');
+    button.on('click', function(e) {
+        if ( slider.hasClass('closed') ) {
+            slider.toggleClass('closed');
+        } else {
+            slider.toggleClass('closed');
+        }
+    });
+});
+
+$( document ).ready( function() {
+    var button = $('.button2');
+    var slider = $('.slider2');
+    button.on('click', function(e) {
+        if ( slider.hasClass('closed') ) {
+            slider.toggleClass('closed');
+        } else {
+            slider.toggleClass('closed');
+        }
+    });
+});
+
+$( document ).ready( function() {
+    var button = $('.button3');
+    var slider = $('.slider3');
+    button.on('click', function(e) {
+        if ( slider.hasClass('closed') ) {
+            slider.toggleClass('closed');
+        } else {
+            slider.toggleClass('closed');
+        }
+    });
+});
